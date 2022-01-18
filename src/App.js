da97656859d92be8c97a5a5d5d0d0e4f047415be
Eddiebee <edbeeotudor@gmail.com>
2022-01-18 13:50:18 +0100

    Complete build

--- src/App.js
+++ src/App.js
@@ -1,27 +1,63 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [action, setAction] = useState({
+    isIncrement: false,
+    isDecrement: false,
+    isReset: true,
+  });
+  // const [isIncrement, setIsIncrement] = useState(false);
 
   const handleIncrement = () => {
+    const isIncrement = true,
+      isDecrement = false,
+      isReset = false;
     setCount(count + 1);
-  }
+    setAction({ isIncrement, isDecrement, isReset });
+  };
   const handleDecrement = () => {
+    const isIncrement = false,
+      isDecrement = true,
+      isReset = false;
     setCount(count - 1);
-  }
+    setAction({ isIncrement, isDecrement, isReset });
+  };
   const handleReset = () => {
+    const isIncrement = false,
+      isDecrement = false,
+      isReset = true;
     setCount(0);
-  }
+    setAction({ isIncrement, isDecrement, isReset });
+  };
+
+  const { isIncrement, isDecrement } = action;
 
   return (
     <div className="App">
       <header className="App-header">
-        Counter with "State Hook"
+        <h1>Counter with "State Hook"</h1>
       </header>
-        <p>You clicked {count} times.</p>
-        <button onClick={handleIncrement}>Increment</button>
-        <button onClick={handleDecrement}>Decrement</button>
-        <button onClick={handleReset}>Reset</button>
+      <p
+        style={
+          isIncrement
+            ? { color: "rgba(20, 202, 51, 0.945)" }
+            : isDecrement
+            ? { color: "rgba(202, 20, 20, 0.945)" }
+            : { color: "rgba(20, 144, 202, 0.945)" }
+        }
+      >
+        {count}
+      </p>
+      <button onClick={handleIncrement} id="increment">
+        Increment
+      </button>
+      <button onClick={handleDecrement} id="decrement">
+        Decrement
+      </button>
+      <button onClick={handleReset} id="reset">
+        Reset
+      </button>
     </div>
   );
 }
